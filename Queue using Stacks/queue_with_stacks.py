7c749c10ab15ffcfe84ab0da556f153bf5eb2f76
andrew-antoniuk <a.antoniuk.pn@ucu.edu.ua>
2026-04-07 01:24:00 +0300

    Copy & Update interface for Queue using Stacks and tests results

---
+++ Queue using Stacks/queue_with_stacks.py
@@ -0,0 +1,72 @@
+"""
+Docstring for queue_with_stacks
+"""
+
+from datatypes import Stack # does not work somehow
+
+class MyQueue:
+
+    """
+    Docstring for MyQueue
+    """
+
+    def __init__(self):
+        self.stack_in = Stack()
+        self.stack_out = Stack()
+
+    def push(self, x: int) -> None:
+
+        """
+        Add item to the back of the queue
+        """
+
+        if not isinstance(x, int):
+            raise TypeError("Incorrect data type for push")
+
+        self.stack_in.push(x)
+
+    def transfer(self):
+
+        """
+        Helper method.
+        """
+
+        if self.stack_out.is_empty():
+            while not self.stack_in.is_empty():
+                self.stack_out.push(self.stack_in.pop())
+
+    def pop(self) -> int:
+
+        """
+        Remove and return item from the front of the queue
+        """
+
+        if self.empty():
+            raise IndexError("Queue is empty")
+
+        self.transfer()
+        return self.stack_out.pop()
+
+    def peek(self) -> int:
+
+        """
+        Return item from the front of the queue
+        """
+
+        self.transfer()
+        return self.stack_out.peek()
+
+    def empty(self) -> bool:
+
+        """
+        Check emptiness of the queue
+        """
+
+        return self.stack_in.is_empty() and self.stack_out.is_empty()
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
