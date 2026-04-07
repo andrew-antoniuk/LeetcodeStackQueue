7dc555ab8972c73be67a2e4470f78666bae20256
andrew-antoniuk <a.antoniuk.pn@ucu.edu.ua>
2026-04-07 16:55:34 +0300

    Frequency Stack Done & Tests Results

--- max_frequency_stack/freq_stack.py
+++ max_frequency_stack/freq_stack.py
@@ -11,7 +11,9 @@ class FreqStack:
     """
 
     def __init__(self):
-        self.stack = Stack()
+        self.freqs = {}
+        self.group = {}
+        self.max_freq = 0 # current
 
     def push(self, val: int) -> None:
 
@@ -19,13 +21,29 @@ class FreqStack:
         Docstring for push
         """
 
-        if not isinstance(val, int):
-            raise TypeError("Incorrect data type for push")
+        # current freq
+        freq = self.freqs.get(val, 0) + 1
+        self.freqs[val] = freq
 
-        self.stack.push(val)
+        self.max_freq = max(self.max_freq, freq)
+
+        # add number to the stack
+        if freq not in self.group:
+            self.group[freq] = Stack() # new stack for new freq
+
+        self.group[freq].push(val)
 
     def pop(self) -> int:
 
         """
         Docstring for pop
         """
+
+        val = self.group[self.max_freq].pop()
+        self.freqs[val] -= 1
+
+        # decrease max_freq if its stack is empty
+        if self.group[self.max_freq].is_empty():
+            self.max_freq -= 1
+
+        return val
